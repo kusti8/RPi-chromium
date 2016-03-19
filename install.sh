fb481c263d67306e3cf442ad3b28ac06453d1fb5
kusti8 <kusti8@gmail.com>
2016-03-19 15:52:48 -0400

    Add youtube-dl update script

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ sudo apt-get install git omxplayer -y
 git clone git://github.com/rg3/youtube-dl
 cd /usr/bin
 sudo ln -s /home/pi/RPi-chromium/youtube-dl/youtube_dl/__main__.py youtube-dl
-sudo ln -s /home/pi/RPi-chromium/native/run_omxplayer.py /usr/local/bin/run_omxplayer.py
+cat > update-youtube.sh << EOF
+
+sudo ln -s /home/pi/RPi-chromium/native/run_omxplayer.py /usr/bin/run_omxplayer.py
 sudo mkdir /etc/chromium-browser/native-messaging-hosts
 sudo cp /home/pi/RPi-chromium/native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
 echo "Done! Now install the RPi-youtube extension."
