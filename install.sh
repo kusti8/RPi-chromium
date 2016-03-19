43faae2e5aa9b7aabd468cea6c5e891b82b79400
kusti8 <kusti8@gmail.com>
2016-03-19 15:54:17 -0400

    Add script
    Former-commit-id: 597ad977dc66199c97f96257da99abb4bdfcfa80

--- install.sh
+++ install.sh
@@ -3,7 +3,26 @@ git clone git://github.com/rg3/youtube-dl
 cd /usr/bin
 sudo ln -s /home/pi/RPi-chromium/youtube-dl/youtube_dl/__main__.py youtube-dl
 cat > update-youtube.sh << EOF
-
+#!/bin/sh
+file=__main__.py
+maxsize=1000
+if [ -d ~/youtube-dl ]
+then
+cd ~/youtube-dl
+git pull
+cd youtube_dl
+actualsize=$(wc -c "$file" | cut -f 1 -d ' ')
+if [ $actualsize -ge $maxsize ]
+then
+wget -O __main__.py https://raw.githubusercontent.com/rg3/youtube-dl/master/youtube_dl/__main__.py
+chmod +x __main__.py
+else
+echo "main file ok"
+fi
+else
+echo "youtube-dl from github not found"
+fi
+EOF
 sudo ln -s /home/pi/RPi-chromium/native/run_omxplayer.py /usr/bin/run_omxplayer.py
 sudo mkdir /etc/chromium-browser/native-messaging-hosts
 sudo cp /home/pi/RPi-chromium/native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
