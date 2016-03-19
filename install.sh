4ff4364744ce42ebd983bb84f6e20ade4cf381ff
kusti8 <kusti8@gmail.com>
2016-03-19 18:42:37 -0400

    Change to EOL

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt-get install git omxplayer -y
 git clone git://github.com/rg3/youtube-dl
 cd /usr/bin
 sudo ln -s /home/pi/RPi-chromium/youtube-dl/youtube_dl/__main__.py youtube-dl
-cat > update-youtube.sh << EOF
+cat > update-youtube.sh << EOL
 #!/bin/sh
 file=__main__.py
 maxsize=1000
@@ -22,7 +22,7 @@ fi
 else
 echo "youtube-dl from github not found"
 fi
-EOF
+EOL
 sudo ln -s /home/pi/RPi-chromium/native/run_omxplayer.py /usr/bin/run_omxplayer.py
 sudo mkdir /etc/chromium-browser/native-messaging-hosts
 sudo cp /home/pi/RPi-chromium/native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
