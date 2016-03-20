e0573002e6c9d977eca8680d2f3fca2f4a714a36
kusti8 <kusti8@gmail.com>
2016-03-20 09:25:48 -0400

    Remove explicit pi user

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 sudo apt-get install git omxplayer -y
 git clone git://github.com/rg3/youtube-dl
+CURRENT=$(pwd)
 cd /usr/bin
-sudo ln -s /home/pi/RPi-chromium/youtube-dl/youtube_dl/__main__.py youtube-dl
+sudo ln -s "$CURRENT/RPi-chromium/youtube-dl/youtube_dl/__main__.py" youtube-dl
 cat > update-youtube.sh << EOF
 #!/bin/sh
 file=__main__.py
 maxsize=1000
-if [ -d ~/youtube-dl ]
+if [ -d "$CURRENT/RPi-chromium/youtube-dl" ]
 then
-cd ~/youtube-dl
+cd "$CURRENT/RPi-chromium/youtube-dl"
 git pull
 cd youtube_dl
 actualsize=$(wc -c "$file" | cut -f 1 -d ' ')
@@ -23,7 +24,7 @@ else
 echo "youtube-dl from github not found"
 fi
 EOF
-sudo ln -s /home/pi/RPi-chromium/native/run_omxplayer.py /usr/bin/run_omxplayer.py
+sudo ln -s "$CURRENT/RPi-chromium/native/run_omxplayer.py" /usr/bin/run_omxplayer.py
 sudo mkdir /etc/chromium-browser/native-messaging-hosts
-sudo cp /home/pi/RPi-chromium/native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
+sudo cp "$CURRENT/RPi-chromium/native/run_omx.json" /etc/chromium-browser/native-messaging-hosts/run_omx.json
 echo "Done! Now install the RPi-youtube extension."
