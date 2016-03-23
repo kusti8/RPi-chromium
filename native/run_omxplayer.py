c7b5ef253e6bc5b5d4d6e69efa14d1ace82ac40b
kusti8 <kusti8@gmail.com>
2016-03-23 18:01:17 -0400

    Remove auto update, add update switch
    Former-commit-id: e26199bd9b1d1452541af21eb515412f54a8fcb4

--- native/run_omxplayer.py
+++ native/run_omxplayer.py
@@ -8,12 +8,25 @@ import os
 
 VERSION=0.4
 
+if sys.argv[1] == "-U":
+    check_update()
+    print "Done with the update!"
+    sys.exit(0)
+elif len(sys.argv) == 1:
+    pass
+else:
+    print "Invalid command line option!\nTo update: run_omxplayer.py -U"
+    
 def check_update():
     new_py = urllib2.urlopen("https://raw.githubusercontent.com/kusti8/RPi-chromium/master/native/run_omxplayer.py").read()
     old_py = open("/usr/bin/run_omxplayer.py").read()
     if new_py is not old_py:
         open("/usr/bin/run_omxplayer.py", 'w').write(new_py)
-    subprocess.call("update-youtube.sh", shell=True)
+    new_man = urllib2.urlopen("https://raw.githubusercontent.com/kusti8/RPi-chromium/master/native/run_omx.json").read()
+    old_man = open("/etc/chromium-browser/native-messaging-hosts/run_omx.json")
+    if new_man is not old_man
+        open("/etc/chromium-browser/native-messaging-hosts/run_omx.json", "w").write(new_man)
+    subprocess.call("update-ytdl", shell=True)
 
 def read_thread_func():
   message_number = 0
@@ -29,4 +42,3 @@ def read_thread_func():
 url = json.loads(read_thread_func())['text']
 print 'OK'
 subprocess.call("omxplayergui.py ytdl " + url, shell=True)
-check_update()
