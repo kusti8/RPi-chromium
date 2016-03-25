9a3184747248d8a4c5776dee0644e7688123cb9a
kusti8 <kusti8@gmail.com>
2016-03-25 19:01:02 -0400

    Add click icon functionality

--- RPi-youtube/background.js
+++ RPi-youtube/background.js
@@ -8,3 +8,13 @@ chrome.extension.onMessage.addListener(
       { text: url })
       })
     })
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        console.log("Got click")
+        console.log(url)
+        chrome.runtime.sendNativeMessage('run_omx',
+      { text: url })
+      })
+});
