2e310fbf6436e0fb4196a5ac16b6ced0301bf3f5
kusti8 <kusti8@gmail.com>
2016-03-25 09:44:42 -0400

    Add temporary omxplayerGUI install options
    Former-commit-id: 462b5d7843a1f8a3dae113e46a1f223543dc73c3

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@ sudo -s <<EOF
 apt-get install omxplayer -y
 CURRENT=$(pwd)
 HOME = $Y_LOC
-wget **URL**
-tar -xzf **FILE**
-cd **FOLDER**
-./installomxplayergui
+wget http://steinerdatenbank.de/software/kweb-1.6.9.tar.gz
+tar -xzf kweb-1.6.9.tar.gz
+cd kweb-1.6.9
+./debinstall
 ginstall-ytdl
+wget http://steinerdatenbank.de/software/omxplayergui-1.7-beta-5.tar.gz
+tar -xzf omxplayergui-1.7-beta-5.tar.gz
+cd omxplayergui-1.7-beta-5
+sudo ./install
 cp "$CURRENT/native/run_omxplayer.py" /usr/bin/run_omxplayer.py
 mkdir /etc/chromium-browser/native-messaging-hosts
 cp "$CURRENT/native/run_omx.json" /etc/chromium-browser/native-messaging-hosts/run_omx.json
