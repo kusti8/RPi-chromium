16d91c2b8019dd8eebf49a1e7e202f248e5f4265
kusti8 <kusti8@gmail.com>
2016-03-25 18:36:22 -0400

    Move up two directories for copying
    Former-commit-id: e8ee4bf41d82775fa609e02aa2c0cebc35153f86

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ tar -xzf omxplayergui-1.7-beta-5.tar.gz
 cd omxplayergui-1.7-beta-5
 sudo ./install
 sudo -s <<EOF
+cd ..
+cd ..
 cp native/run_omxplayer.py /usr/bin/run_omxplayer.py
 mkdir /etc/chromium-browser/native-messaging-hosts
 cp native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
