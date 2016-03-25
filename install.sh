8f1342a5c491bd6149206384864474c4ee80b916
kusti8 <kusti8@gmail.com>
2016-03-25 10:11:06 -0400

    Remove current and use home directory

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 Y_LOC=$HOME
 sudo -s <<EOF
 apt-get install omxplayer -y
-CURRENT=$(pwd)
 HOME=$Y_LOC
 wget http://steinerdatenbank.de/software/kweb-1.6.9.tar.gz
 tar -xzf kweb-1.6.9.tar.gz
@@ -12,8 +11,8 @@ wget http://steinerdatenbank.de/software/omxplayergui-1.7-beta-5.tar.gz
 tar -xzf omxplayergui-1.7-beta-5.tar.gz
 cd omxplayergui-1.7-beta-5
 sudo ./install
-cp "$CURRENT/native/run_omxplayer.py" /usr/bin/run_omxplayer.py
+cp ~/RPi-chromium/native/run_omxplayer.py" /usr/bin/run_omxplayer.py
 mkdir /etc/chromium-browser/native-messaging-hosts
-cp "$CURRENT/native/run_omx.json" /etc/chromium-browser/native-messaging-hosts/run_omx.json
+cp ~/RPi-chromium/native/run_omx.json" /etc/chromium-browser/native-messaging-hosts/run_omx.json
 echo "Done! Now install the RPi-youtube extension."
 EOF
