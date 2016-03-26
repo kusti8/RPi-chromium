45cb787ecb51f9133e11dbaffd8af04620953352
kusti8 <kusti8@gmail.com>
2016-03-26 11:43:17 -0400

    Add test for update

--- install-chromium.sh
+++ install-chromium.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Download and install
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
