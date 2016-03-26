52d6689663fb0dd622e3ad4e5871ae304ba9e7b4
kusti8 <kusti8@gmail.com>
2016-03-25 20:04:12 -0400

    Make run_omxplayer.py executable
    Former-commit-id: 366ecc08cdf0f45bfe05dbd42cbe990d727ccd6c

--- install.sh
+++ install.sh
@@ -16,5 +16,6 @@ cd ..
 cp native/run_omxplayer.py /usr/bin/run_omxplayer.py
 mkdir /etc/chromium-browser/native-messaging-hosts
 cp native/run_omx.json /etc/chromium-browser/native-messaging-hosts/run_omx.json
+chmod +x /usr/bin/run_omxplayer.py
 echo "Done! Now install the RPi-youtube extension."
 EOF
