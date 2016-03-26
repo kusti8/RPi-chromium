f249fc037f07dffb65f2f11f178439517001918d
kusti8 <kusti8@gmail.com>
2016-03-26 14:50:09 -0400

    Make install-chromium.sh executable

--- native/run_omxplayer.py
+++ native/run_omxplayer.py
@@ -25,7 +25,7 @@ def check_update():
     new_chrome = urllib2.urlopen("https://raw.githubusercontent.com/kusti8/RPi-chromium/master/install-chromium.sh").read()
     if new_chrome is not old_chrome:
         open("install-chromium.sh", 'w').write(new_chrome)
-        subprocess.call("sudo mv install-chromium.sh /usr/bin/install-chromium.sh && install-chromium.sh", shell=True)
+        subprocess.call("sudo mv install-chromium.sh /usr/bin/install-chromium.sh && sudo chmod +x /usr/bin/install-chromium.sh && install-chromium.sh", shell=True)
 
 
 #open('/home/pi/test', 'w').write(''.join(sys.argv))
