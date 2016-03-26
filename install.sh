1ef717ad26e0569a3090b64068687c1c071b32fe
kusti8 <kusti8@gmail.com>
2016-03-25 22:22:06 -0400

    Add fi

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ if [ ! -d /etc/chromium-browser ]; then
   wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
   dpkg -i chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
   dpkg -i chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+fi
 cd ..
 cd ..
 cp native/run_omxplayer.py /usr/bin/run_omxplayer.py
