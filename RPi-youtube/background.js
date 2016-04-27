851ac02ea2c3bfec9ef2a50ecce9b35cc1fd55fd
kusti8 <kusti8@gmail.com>
2016-04-27 17:14:21 -0400

    Add initial pause functionality

--- RPi-youtube/background.js
+++ RPi-youtube/background.js
@@ -9,9 +9,17 @@ chrome.extension.onMessage.addListener(
       })
     })
 
+function matchRuleShort(str, rule) {
+    return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+}
+
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
+        if (url === matchRuleShort(url, '*://*youtube.com/watch*')) {
+            chrome.tabs.executeScript(null, {file: "jq.js"})
+            chrome.tabs.executeScript({code: "$('video').pause()"})
+        }
         console.log("Got click")
         console.log(url)
         chrome.runtime.sendNativeMessage('run_omx',
