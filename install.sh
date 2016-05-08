12e2cdff01f41e7d23a34b7d48eb627089bdd743
kusti8 <kusti8@gmail.com>
2016-05-08 09:39:33 -0400

    Add local developer installs

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
-wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
-echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install chromium-browser rpi-youtube -y
+if grep -q "http://dl.bintray.com/kusti8/chromium-rpi" /etc/apt/sources.list; then
+  sudo apt-get update
+  sudo apt-get install chromium-browser kweb -y
+else
+  wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+  echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list
+  sudo apt-get update
+  sudo apt-get install chromium-browser kweb -y
+  
+sudo cp native/run_omxplayer.py /usr/bin/
+sudo cp native/start_ytdl_server.sh /usr/bin
+sudo mkdir /etc/chromium-browser/native-messaging-hosts
+sudo cp native/run_omx.json /etc/chromium-browser/native-messaging-hosts/
+chromium-browser chrome://extensions &
+sudo leafpad /etc/chromium-browser/native-messaging-hosts/run_omx.json
+
